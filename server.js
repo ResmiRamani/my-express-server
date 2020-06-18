946ddee08b3249535836956cd5f84a123703dfc7
Resmi Ramani <Resmi.RamaniAmma@triosstudent.com>
2020-06-18 14:33:33 -0400

    Handling Requests and Response:the GET Request

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+app.get("/", function (request, response) {
+  response.send("<h1>Hello,World!</h1>");
+});
 app.listen(3000, function () {
   console.log("Server  started on port 3000");
 });
